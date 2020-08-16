e7798faec26b01ab6a5e581861bac019216aea6a
lalebdi <loai.alebdi@gmail.com>
2020-08-16 13:59:51 -0400

    done with users management and error handling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -12,12 +14,22 @@ const io = socketio(server);
 
 // to register clients entering and leaving is below:
 io.on('connection', (socket) =>{
-    console.log('We have a new connection!!!');
+    // console.log('We have a new connection!!!');
 
     socket.on('join', ({ name, room }, callback) =>{
-        console.log(name, room);
+        // console.log(name, room);
         // the call back is for error handling
+        const { error, user } = addUser({ id: socket.id, name, room}); 
         
+        if(error) return callback(error); // the error message is coming from the functions in the users.js
+
+        socket.emit('message', { user: 'admin' , text: `${user.name}, welcome to the room ${user.room}  ` }); //this is for admin messaging
+
+        socket.broadcast.to(user.room).emit('message', { user:'admin', text: `${user.name}, has joined!`}); // this is for sending a message to everyone beside the specific user. 
+
+        socket.join(user.room);
+
+        callback();
     })
 
     socket.on('disconnect', ()=> {
