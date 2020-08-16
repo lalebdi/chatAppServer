ddb0d5890b80ad3b7f5271ffe5b6af41fabe22c1
lalebdi <loai.alebdi@gmail.com>
2020-08-16 12:26:31 -0400

    we can register clients entering and leaving

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// to register clients entering and leaving is below:
+io.on('connection', (socket) =>{
+    console.log('We have a new connection!!!')
+
+    socket.on('disconnect', ()=> {
+        console.log('User had left!!')
+    })
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
